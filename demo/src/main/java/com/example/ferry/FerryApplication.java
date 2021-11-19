2aec3c464ac4c145d8f9a8c4e637fa66096b057c
Ferry <Ferrysharma333@gmail.com>
2021-11-18 21:48:07 -0800

    my code

--- demo/src/main/java/com/example/ferry/FerryApplication.java
+++ demo/src/main/java/com/example/ferry/FerryApplication.java
@@ -1,10 +1,13 @@
 package com.example.ferry;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+@Slf4j
 public class FerryApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
@@ -16,7 +19,7 @@ public class FerryApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		System.out.println("Hello World");
+		log.info("Hello World");
 
 	}
 }
